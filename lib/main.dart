6467772194135b34e8bde472b4dc623452302751
NaderSameer <nadersameer@std.mans.edu.eg>
2026-01-29 18:15:48 +0200

    Finish of ios & Android Configration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const FlutterGoogleMapsIntegration());
 }
 
-class FlutterGoogleMapsIntegration extends StatelessWidget {
+class FlutterGoogleMapsIntegration extends StatefulWidget {
   const FlutterGoogleMapsIntegration({super.key});
 
   @override
+  State<FlutterGoogleMapsIntegration> createState() =>
+      _FlutterGoogleMapsIntegrationState();
+}
+
+class _FlutterGoogleMapsIntegrationState
+    extends State<FlutterGoogleMapsIntegration> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Flutter google maps Integration'),
+        body: GoogleMap(
+          onMapCreated: (controller) {},
+          initialCameraPosition: CameraPosition(
+            zoom: 16,
+            target: LatLng(
+              31.12397562358649,
+              31.28898459645746,
+            ),
+          ),
         ),
       ),
     );
