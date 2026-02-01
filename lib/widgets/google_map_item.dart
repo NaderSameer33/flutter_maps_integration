3f4ef39ae789a760e91f977f408a651b385ffa9b
NaderSameer <nadersameer@std.mans.edu.eg>
2026-02-01 23:01:01 +0200

    Adding polyLine & Test it

--- lib/widgets/google_map_item.dart
+++ lib/widgets/google_map_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_google_maps_intgration/models/map_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:ui' as ui;
 
 class GoogleMapItem extends StatefulWidget {
   const GoogleMapItem({super.key});
@@ -15,13 +17,16 @@ class _GoogleMapItemState extends State<GoogleMapItem> {
 
   String mapStyle = '';
   Set<Marker> markers = {};
+  Set<Polyline> polylines = {};
+
   @override
   void initState() {
     super.initState();
+    createPlyLine();
     createMarker();
     createMapStyle();
     cameraPosition = CameraPosition(
-      zoom: 13,
+      zoom: 1,
       target: LatLng(
         31.12397562358649,
         31.28898459645746,
@@ -36,24 +41,20 @@ class _GoogleMapItemState extends State<GoogleMapItem> {
     setState(() {});
   }
 
-  Future<BitmapDescriptor> createCustomMarker() async {
-    final customMarker = await BitmapDescriptor.asset(
-      ImageConfiguration(
-        size: Size(20, 20),
-        platform: TargetPlatform.android,
-        devicePixelRatio: 10,
-      ),
+  Future<BitmapDescriptor> createCutomMarker() async {
+    var customMarker = await BitmapDescriptor.asset(
+      ImageConfiguration(size: Size(20, 20)),
       'assets/images/marker2.png',
     );
     return customMarker;
   }
 
   void createMarker() async {
-    var markerIcon = await createCustomMarker();
+    var icon = await createCutomMarker();
     var myMarker = list
         .map(
           (e) => Marker(
-            icon: markerIcon,
+            icon: icon,
             infoWindow: InfoWindow(
               title: e.name,
             ),
@@ -76,12 +77,31 @@ class _GoogleMapItemState extends State<GoogleMapItem> {
     );
   }
 
+  void createPlyLine() {
+    var polyLineData = polyLineList
+        .map(
+          (model) => Polyline(
+            geodesic: true,
+            zIndex: 10,
+            startCap: Cap.roundCap,
+            color: Colors.red,
+            width: 5,
+            polylineId: PolylineId(model.id),
+            points: model.lating,
+          ),
+        )
+        .toSet();
+    polylines.addAll(polyLineData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           GoogleMap(
+            polylines: polylines,
+            zoomControlsEnabled: false,
             markers: markers,
             style: mapStyle,
             onMapCreated: (controller) {
@@ -103,3 +123,28 @@ class _GoogleMapItemState extends State<GoogleMapItem> {
     );
   }
 }
+
+class PolylineModel {
+  final String id;
+  final List<LatLng> lating;
+  PolylineModel(this.id, this.lating);
+}
+
+List<PolylineModel> polyLineList = [
+  PolylineModel('neshar&nabaruh', [
+    LatLng(31.12713961543846, 31.291574473598246),
+    LatLng(31.097206838135566, 31.30576955578635),
+  ]),
+  PolylineModel('nabaruh&byla', [
+    LatLng(31.097206838135566, 31.30576955578635),
+    LatLng(31.170558599682327, 31.222880131522405),
+  ]),
+  PolylineModel('aldeen & abdeljany', [
+    LatLng(31.104233589178055, 31.269490481110104),
+    LatLng(31.121869722880067, 31.317384002859825),
+  ]),
+  PolylineModel('africa & turkia', [
+    LatLng(-32.43403248415015, 22.651472347820484),
+    LatLng(70.81735656288016, 127.2481522340003),
+  ]),
+];
